515ce349f756d755c94cb4a741655439e63465d9
Kyle Hekkers <kyle.hekkers@gmail.com>
2021-06-30 09:49:11 -0500

    Repository

---
+++ src/Lead2Change.Repositories/CareerDeclarationRepository/ICareerDeclarationRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Lead2Change.Domain.Models;
+
+namespace Lead2Change.Repositories.CareerDeclarationRepository
+{
+    interface ICareerDeclarationRepository
+    {
+        public Task<CareerDeclaration> Create(CareerDeclaration careerDeclaration);
+        public Task<List<CareerDeclaration>> GetCareerDeclarations();
+        public Task<CareerDeclaration> GetCareerDeclaration(Guid id);
+        public Task<CareerDeclaration> Update(CareerDeclaration model);
+        public Task<CareerDeclaration> Delete(CareerDeclaration model);
+    }
+}
