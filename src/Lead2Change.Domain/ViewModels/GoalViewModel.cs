25896f90169406dfd37a6deff9625eabd72e8467
Conlan <cgotzion@gmail.com>
2021-06-30 09:35:09 -0500

    Goal View Model

--- src/Lead2Change.Domain/ViewModels/GoalViewModel.cs
+++ src/Lead2Change.Domain/ViewModels/GoalViewModel.cs
@@ -9,5 +9,13 @@ namespace Lead2Change.Domain.ViewModels
     {
         [Required]
         public string Goal { get; set; }
+        public Guid Id { get; set; }
+        public Guid StudentId { get; set; }
+        public string GoalSet { get; set; }
+        public DateTime DateGoalSet { get; set; }
+        public string SEL { get; set; }
+        public DateTime GoalReviewDate { get; set; }
+        public string WasItAccomplished { get; set; }
+        public string Explanation { get; set; }
     }
 }
