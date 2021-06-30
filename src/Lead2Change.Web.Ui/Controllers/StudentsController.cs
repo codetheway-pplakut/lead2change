b2b2f8d717e288f7d62da327d94acff70c5e6987
Benjamin Kim <22kimb@elmbrookstudenst.org>
2021-06-30 10:53:18 -0500

    Started on Controller

--- src/Lead2Change.Web.Ui/Controllers/StudentsController.cs
+++ src/Lead2Change.Web.Ui/Controllers/StudentsController.cs
@@ -2,6 +2,8 @@
 using Lead2Change.Services.Identity;
 using Lead2Change.Services.Students;
 using Microsoft.AspNetCore.Mvc;
+using Lead2Change.Domain.Models;
+using Lead2Change.Domain.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +18,56 @@ namespace Lead2Change.Web.Ui.Controllers
         public StudentsController(IIdentityService identityService, IStudentService studentService) : base(identityService)
         {
             _studentService = studentService;
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View(new RegistrationViewModel());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                    Student student = new Student()
+                    {
+        /*                public Guid Id { get; set; }
+        public string StudentFirstName { get; set; }
+        public string StudentLastName { get; set; }
+        public DateTime StudentDateOfBirth { get; set; }
+        public string StudentAddress { get; set; }
+        public string StudentApartmentNumber { get; set; }
+        public string StudentCity { get; set; }
+        public int StudentZipCode { get; set; }
+        public int StudentHomePhone { get; set; }
+        public int StudentCellPhone { get; set; }
+        public string StudentEmail { get; set; }
+        public string StudentCareerPath { get; set; }
+        public string StudentCareerInterest { get; set; }
+
+
+        public string ParentFirstName { get; set; }
+        public string ParentLastName { get; set; }
+        public string Address { get; set; }
+
+        public string ParentCity { get; set; }
+        public string ParentState { get; set; }
+        public int ParentZipCode { get; set; }
+        public int ParentHomePhone { get; set; }
+        public int ParentCellPhone { get; set; }
+        public string ParentEmail { get; set; }
+
+        public bool KnowGuidanceCounselor { get; set; }
+        public string GuidanceCounselorName { get; set; }
+        public bool MeetWithGuidanceCounselor { get; set; }
+        public string HowOftenMeetWithGuidanceCounselor { get; set; }
+        public string DiscussWithGuidanceCounselor { get; set; }
+        */
+    };
+                    var abc = await StudentService.Create(student);
+                return RedirectToAction("Index");
+            }
+            return View(model);
         }
     }
 }
