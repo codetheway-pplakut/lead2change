a29de55be4fc28f06cac4bbfcf3c656000668a10
Anna <22huanga@elmbrookstudents.org>
2021-07-14 14:54:34 -0500

    Basic CRUD Operations

---
+++ src/Lead2Change.Domain/ViewModels/QuestionsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Lead2Change.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Lead2Change.Domain.ViewModels
+{
+    public class QuestionsViewModel
+    {
+        public string QuestionString { get; set; }
+        public Guid Id { get; set; }
+        public ICollection<QuestionInInterview> QuestionInInterviews { get; set; }
+    }
+}
---
+++ src/Lead2Change.Services/Questions/QuestionsService.cs
@@ -0,0 +1,44 @@
+﻿using Lead2Change.Data.Contexts;
+using Lead2Change.Domain.Constants;
+using Lead2Change.Domain.Models;
+using Lead2Change.Domain.ViewModels;
+using Lead2Change.Repositories.AppEvents;
+using Lead2Change.Repositories.Questions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lead2Change.Services.Questions
+{
+    public class QuestionsService : IQuestionsService
+    {
+        private IQuestionsRepository QuestionsRepository;
+
+        public QuestionsService(AppDbContext dbContext)
+        {
+            this.QuestionsRepository = new QuestionsRepository(dbContext);
+        }
+        public async Task<List<Question>> GetQuestions()
+        {
+            return await this.QuestionsRepository.GetQuestions();
+
+        }
+        public async Task<Question> GetQuestion(Guid id)
+        {
+            return await this.QuestionsRepository.GetQuestion(id);
+        }
+        public async Task<Question> Update(Question model)
+        {
+            return await this.QuestionsRepository.Update(model);
+        }
+        public async Task<Question> Create(Question question)
+        {
+            return await this.QuestionsRepository.Create(question);
+        }
+        public async Task<Question> Delete(Question question)
+        {
+            return await QuestionsRepository.Delete(question);
+        }
+    }
+}
