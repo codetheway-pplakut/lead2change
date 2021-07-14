6f3fc5dc7e25b38317ef3dca8484e7ce2ab6fd50
Joel Kuriakose <1joel.kuriakose@gmail.com>
2021-07-14 09:53:32 -0500

    Email Confirmation for student, parent, and admin

--- src/Lead2Change.Web.Ui/Controllers/_BaseController.cs
+++ src/Lead2Change.Web.Ui/Controllers/_BaseController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace Lead2Change.Web.Ui.Controllers
 {
@@ -15,5 +17,22 @@ namespace Lead2Change.Web.Ui.Controllers
         {
             _identityService = identityService;
         }
+
+        public async Task Email(string sender, string receiver, string xSubject, string xPlainTextContent, string xHtmlContent, string senderTitle, string receiverTitle)
+        {
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(sender, senderTitle);
+            var subject = xSubject;
+
+            var to = new EmailAddress(receiver, receiverTitle);
+            var plainTextContent = xPlainTextContent;
+            var htmlContent = xHtmlContent;
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
+
+
     }
 }
+
