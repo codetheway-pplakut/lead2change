964463a55b8268ba7c164a337b2a59889302c49a
Kevin Niu <kevin.h.niu@gmail.com>
2021-07-14 10:46:40 -0500

    Srarted conditional checkboxes

--- src/Lead2Change.Web.Ui/wwwroot/js/site.js
+++ src/Lead2Change.Web.Ui/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function showHideClass(id1) {
+    var x = document.getElementsByClassName(id1);
+    for (let i = 0; i < x.length; i++) {
+        if (x[i].style.display !== "none") {
+            x[i].style.display = "none";
+        } else {
+            x[i].style.display = "block";
+        }
+    }
+}
+
+function showHideClass2(id2) {
+    var x = document.getElementsByClassName(id2);
+    for (let i = 0; i < x.length; i++) {
+        if (x[i].style.display !== "none") {
+            x[i].style.display = "none";
+        } else {
+            x[i].style.display = "block";
+        }
+    }
+}
