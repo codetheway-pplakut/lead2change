1a2a5235eec7b44ced027105252ff01c55dbcff5
Anna <22huanga@elmbrookstudents.org>
2021-07-15 14:59:15 -0500

    Question Styling v1

--- src/Lead2Change.Domain/ViewModels/AnswersViewModel.cs
+++ src/Lead2Change.Domain/ViewModels/AnswersViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lead2Change.Domain.ViewModels
 {
     public class AnswersViewModel
     {
+        [Display(Name = "Answer:")]
         public string AnswerString { get; set; }
         public Guid Id { get; set; }
         public Guid StudentId { get; set; }
