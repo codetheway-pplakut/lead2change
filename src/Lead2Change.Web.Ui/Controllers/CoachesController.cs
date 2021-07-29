6aa94a5cac9c3e598d676fa39b477594ff4886cc
Kyle Hekkers <kyle.hekkers@gmail.com>
2021-07-29 12:48:21 -0500

    Formatting

--- src/Lead2Change.Web.Ui/Controllers/CoachesController.cs
+++ src/Lead2Change.Web.Ui/Controllers/CoachesController.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
-
 using Lead2Change.Domain.Constants;
 using Lead2Change.Services.Students;
 
