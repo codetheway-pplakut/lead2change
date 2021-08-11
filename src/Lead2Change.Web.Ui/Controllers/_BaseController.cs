a00708409d4e5e458c9468e3ccb6b9678d8a98a9
Adam Miller <theadammiller56@gmail.com>
2021-08-11 09:22:31 -0500

    Removed Unneccessary Email Method

--- src/Lead2Change.Web.Ui/Controllers/_BaseController.cs
+++ src/Lead2Change.Web.Ui/Controllers/_BaseController.cs
@@ -127,21 +127,5 @@ namespace Lead2Change.Web.Ui.Controllers
         {
             return View("Error", e);
         }
-
-        /* THIS FUNCTION IS A WORK IN PROGRESS
-        protected async Task Email(string sender, string receiver, string xSubject, string xPlainTextContent, string xHtmlContent, string senderTitle, string receiverTitle)
-        {
-            var apiKey = "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";
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress(sender, senderTitle);
-            var subject = xSubject;
-
-            var to = new EmailAddress(receiver, receiverTitle);
-            var plainTextContent = xPlainTextContent;
-            var htmlContent = xHtmlContent;
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);
-        } 
-        */
     }
 }
